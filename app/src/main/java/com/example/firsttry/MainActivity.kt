f3f1751a504e5115ca291bb9c3f6a17fbdfe9978
Gregory <lubeevgs2000@gmail.com>
2025-07-23 08:00:03 +0300

    Initial commit

---
+++ app/src/main/java/com/example/firsttry/MainActivity.kt
@@ -0,0 +1,84 @@
+package com.example.firsttry
+
+import android.graphics.Color
+import android.os.Bundle
+import android.widget.TextView
+import androidx.activity.enableEdgeToEdge
+import androidx.appcompat.app.AppCompatActivity
+import androidx.core.content.ContextCompat
+import androidx.core.view.ViewCompat
+import androidx.core.view.WindowInsetsCompat
+import androidx.core.view.isVisible
+import com.example.firsttry.databinding.ActivityLearnWordBinding
+
+class MainActivity : AppCompatActivity() {
+
+    private var _binding: ActivityLearnWordBinding? = null
+    private val binding
+        get() = _binding
+            ?: throw IllegalStateException("Binding for ActivityLearnWordBinding must not be null")
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        _binding = ActivityLearnWordBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+        binding.layoutAnswer3.setOnClickListener {
+            markAnswerCorrect()
+        }
+
+    }
+
+    private fun markAnswerCorrect() {
+        binding.layoutAnswer3.background = ContextCompat.getDrawable(
+            this@MainActivity,
+            R.drawable.shape_rounded_containers_correct
+        )
+
+        binding.tvVariantNumber3.background = ContextCompat.getDrawable(
+            this@MainActivity,
+            R.drawable.shape_rounded_variants_correct
+        )
+
+        binding.tvVariantNumber3.setTextColor(
+            ContextCompat.getColor(
+                this@MainActivity,
+                R.color.white
+            )
+        )
+
+        binding.tvVariantValue3.setTextColor(
+            ContextCompat.getColor(
+                this@MainActivity,
+                R.color.correctAnswerColor
+            )
+        )
+
+        binding.btnSkip.isVisible = false
+
+        binding.layoutResult.setBackgroundColor(
+            ContextCompat.getColor(
+                this@MainActivity,
+                R.color.correctAnswerColor
+            )
+        )
+
+        binding.ivResultIcon.setImageDrawable(
+            ContextCompat.getDrawable(
+                    this@MainActivity,
+                    R.drawable.ic_correct
+                )
+            )
+
+        binding.tvResultMessage.text = resources.getString(R.string.title_correct)
+
+        binding.btnContinue.setTextColor(
+            ContextCompat.getColor(
+                this@MainActivity,
+                R.color.correctAnswerColor
+            )
+        )
+
+        binding.layoutResult.isVisible = true
+    }
+}
