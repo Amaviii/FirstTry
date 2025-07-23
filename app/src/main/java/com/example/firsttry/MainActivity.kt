db80c13f96eb6bbde5c0fe4183c8cce8a65cf01f
Gregory <lubeevgs2000@gmail.com>
2025-07-23 08:47:11 +0300

    Wrong answer1 added

--- app/src/main/java/com/example/firsttry/MainActivity.kt
+++ app/src/main/java/com/example/firsttry/MainActivity.kt
@@ -1,13 +1,9 @@
 package com.example.firsttry
 
-import android.graphics.Color
+
 import android.os.Bundle
-import android.widget.TextView
-import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.content.ContextCompat
-import androidx.core.view.ViewCompat
-import androidx.core.view.WindowInsetsCompat
 import androidx.core.view.isVisible
 import com.example.firsttry.databinding.ActivityLearnWordBinding
 
@@ -27,6 +23,63 @@ class MainActivity : AppCompatActivity() {
             markAnswerCorrect()
         }
 
+        binding.layoutAnswer1.setOnClickListener {
+            markAnswerWrong()
+        }
+
+    }
+
+    private fun markAnswerWrong() {
+        binding.layoutAnswer1.background = ContextCompat.getDrawable(
+            this@MainActivity,
+            R.drawable.shape_rounded_containers_wrong
+        )
+
+        binding.tvVariantNumber1.background = ContextCompat.getDrawable(
+            this@MainActivity,
+            R.drawable.shape_rounded_variants_wrong
+        )
+
+        binding.tvVariantNumber1.setTextColor(
+            ContextCompat.getColor(
+                this@MainActivity,
+                R.color.white
+            )
+        )
+
+        binding.tvVariantValue1.setTextColor(
+            ContextCompat.getColor(
+                this@MainActivity,
+                R.color.wrongAnswerColor
+            )
+        )
+
+        binding.btnSkip.isVisible = false
+
+        binding.layoutResult.setBackgroundColor(
+            ContextCompat.getColor(
+                this@MainActivity,
+                R.color.wrongAnswerColor
+            )
+        )
+
+        binding.ivResultIcon.setImageDrawable(
+            ContextCompat.getDrawable(
+                this@MainActivity,
+                R.drawable.ic_wrong
+            )
+        )
+
+        binding.tvResultMessage.text = resources.getString(R.string.title_wrong)
+
+        binding.btnContinue.setTextColor(
+            ContextCompat.getColor(
+                this@MainActivity,
+                R.color.wrongAnswerColor
+            )
+        )
+
+        binding.layoutResult.isVisible = true
     }
 
     private fun markAnswerCorrect() {
